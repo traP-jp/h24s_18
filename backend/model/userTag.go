24fcdbe56bbcf42b3c20bbe93550b74b585c40f0
wakky-alcedo <wakky.alcedo@gmail.com>
2024-06-15 15:06:09 +0900

    fix ユーザーが持っているタグをすべて取得するように修正

--- backend/model/userTag.go
+++ backend/model/userTag.go
@@ -17,8 +17,8 @@ func CreateUserTag(userId string, tagName string, isStarred bool) error {
 	return result.Error
 }
 
-func GetUserTagsByUserId(tagName string) ([]UserTag, error) {
+func GetUserTagsByUserId(userId string) ([]UserTag, error) {
 	var userTag []UserTag
-	result := db.Find(&userTag, "tagName = ?", tagName)
+	result := db.Find(&userTag, "userId = ?", userId)
 	return userTag, result.Error
 }
