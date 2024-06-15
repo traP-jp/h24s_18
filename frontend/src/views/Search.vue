2e6603d30dc9fbaa61c727824189cca17cf27bed
mumumu <takumu.sato@gmail.com>
2024-06-15 14:21:32 +0900

    tagをボタンにしました

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
+
 interface User {
   name: string;
-  tag: string;
+  tags: string[]; // tags を文字列の配列として定義する
 }
-// ユーザーの配列の中にタグの配列をいれる
+
 const userData = ref<User[]>([
-  { name: "mumumu", tag: "24B" },
-  { name: "aya_se", tag: "23M" },
-  { name: "eru_o2lo", tag: "24B"}
+  { name: "mumumu", tags: ["24B", "aaa", "ハッカソンなう"] }, // tags を配列で指定する
+  { name: "aya_se", tags: ["23M"] },
+  { name: "eru_o2lo", tags: ["24B"] },
 ]);
 </script>
 
@@ -18,14 +19,19 @@ const userData = ref<User[]>([
     <h2 v-if="$route.query.q">
       <div id="app"># {{ $route.query.q }}</div>
     </h2>
-    
+
     <li v-for="user in userData.slice()" :key="user.name">
-      <h3>{{ user.name }}</h3>
-      <img
-        :src="`https://q.trap.jp/api/v3/public/icon/${user.name}`"
-        alt="アイコン"
-        id="icon"
-      />
+      <div class="username">
+        <h3>{{ user.name }}</h3>
+        <img
+          :src="`https://q.trap.jp/api/v3/public/icon/${user.name}`"
+          alt="アイコン"
+          id="icon"
+        />
+        <div v-for="tag in user.tags.slice()" :key="tag" class="tag">
+            <button>    {{ tag }} </button>
+        </div>
+      </div>
     </li>
   </div>
 </template>
@@ -34,5 +40,13 @@ const userData = ref<User[]>([
 #icon {
   width: 90px;
   border-radius: 50%;
+}
+
+.username {
+  display: inline-block;
+}
+
+.usertag {
+  display: inline-block;
 }
 </style>
