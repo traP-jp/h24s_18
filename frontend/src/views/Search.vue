ae6d61fb0f1625827f7d65c53e187672d6b9e5ef
mumumu <takumu.sato@gmail.com>
2024-06-15 10:44:49 +0900

    URLパラメータを検索タグ名に反映できるようにした

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,8 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const tag = route.query.q;
+</script>
 
 <template>
   <div>
     <h1>検索ページ</h1>
+
+    <div id="app">
+      {{ tag }}
+    </div>
+   
   </div>
 </template>
 
