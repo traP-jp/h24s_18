dc203c480a64c4ddd40180ca71ceca532aa72e0b
aya-se <kakkeno1@gmail.com>
2024-06-15 14:20:23 +0900

    Add Header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { store } from "./store";
-import router from "./router";
-
-const logout = () => {
-  store.user.id = "";
-  router.push({ name: "Login" });
-};
+import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
   <div>
-    <p><strong>ユーザーID：</strong> {{ store.user.id }}</p>
-    <p><strong>パス：</strong> {{ $route.fullPath }}</p>
-    <div v-if="store.user.id">
-      <RouterLink :to="{ name: 'User', params: { id: store.user.id } }"
-        >ユーザーページへ</RouterLink
-      >
-      <br />
-      <RouterLink :to="{ name: 'Search' }">検索ページへ</RouterLink>
-      <br />
-      <button @click="logout">ログアウト</button>
-    </div>
+    <Header />
     <main>
       <RouterView />
     </main>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,9 +1,75 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { store } from "../store";
+import router from "../router";
+
+const logout = () => {
+  store.user.id = "";
+  router.push({ name: "Login" });
+};
+</script>
 
 <template>
-  <div>
-    <button>Sample Button</button>
+  <div class="header">
+    <div v-if="store.user.id">
+      <RouterLink
+        class="header-button"
+        :to="{ name: 'User', params: { id: store.user.id } }"
+        ><strong>traPハッカソン2024春18班</strong></RouterLink
+      >
+    </div>
+    <div v-else>
+      <RouterLink class="header-button" :to="{ name: 'Login' }"
+        ><strong>traPハッカソン2024春18班</strong></RouterLink
+      >
+    </div>
+    <div class="header-contents">
+      <div v-if="store.user.id">
+        <RouterLink
+          class="header-button"
+          :to="{ name: 'User', params: { id: store.user.id } }"
+          >ユーザーページ</RouterLink
+        >
+        <RouterLink class="header-button" :to="{ name: 'Search' }"
+          >検索ページ</RouterLink
+        >
+        <button class="header-button" @click="logout">ログアウト</button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 40px;
+  width: 100%;
+  padding: 0 20px;
+  border-bottom: 1px solid #ccc;
+  padding: 10px 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+.header-contents {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: right;
+  align-items: center;
+  padding-right: 40px;
+}
+.header-button {
+  color: #333;
+  background: none;
+  border: none;
+  cursor: pointer;
+  margin: 0 10px;
+  padding: 0;
+}
+.header-button:hover {
+  color: #005bac;
+}
+</style>
