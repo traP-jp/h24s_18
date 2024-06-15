a57fbf796afc28726c39e078b1d54a05a9d7d6dd
aya-se <kakkeno1@gmail.com>
2024-06-15 13:47:16 +0900

    Add Login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import { store } from "./store";
+import router from "./router";
+
+const logout = () => {
+  store.user.id = "";
+  router.push({ name: "Login" });
+};
 </script>
 
 <template>
   <div>
-    <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-    <RouterLink :to="{ name: 'User', params: { id: store.user.id } }"
-      >ユーザーページへ</RouterLink
-    >
-    <hr />
-    <RouterLink :to="{ name: 'Search' }">検索ページへ</RouterLink>
+    <p><strong>ユーザーID：</strong> {{ store.user.id }}</p>
+    <p><strong>パス：</strong> {{ $route.fullPath }}</p>
+    <div v-if="store.user.id">
+      <RouterLink :to="{ name: 'User', params: { id: store.user.id } }"
+        >ユーザーページへ</RouterLink
+      >
+      <br />
+      <RouterLink :to="{ name: 'Search' }">検索ページへ</RouterLink>
+      <br />
+      <button @click="logout">ログアウト</button>
+    </div>
     <main>
       <RouterView />
     </main>
--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -1,11 +1,8 @@
-<script setup lang="ts">
-import SampleButton from "../components/SampleButton.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>ユーザーページ</h1>
-    <SampleButton />
   </div>
 </template>
 
