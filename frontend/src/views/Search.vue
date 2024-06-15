2f56d190065cc2b7dc6431b4c5c68711710b1f2b
mumumu <takumu.sato@gmail.com>
2024-06-15 10:49:08 +0900

    不要なものを除きました

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,18 +1,12 @@
-<script setup lang="ts">
-import { useRoute } from "vue-router";
-
-const route = useRoute();
-const tag = route.query.q;
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>検索ページ</h1>
 
     <div id="app">
-      {{ tag }}
+      {{ $route.query.q }}
     </div>
-   
   </div>
 </template>
 
